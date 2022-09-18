a03d6cc2bf852c63f43db1544534ede8caf230b2
facundo <facuwilli@gmail.com>
2022-09-18 15:55:23 -0300

    Added abstract class with a method to get a direction based on input.

---
+++ Chicken-Day-Z/Assets/Scripts/Gameplay/Characters/Chicken/Movement/Input/MovementInputDetectionMechanic.cs
@@ -0,0 +1,7 @@
+namespace ChickenDayZ.Gameplay.Characters.Chicken.Movement.Input 
+{
+    public abstract class MovementInputDetectionMechanic
+    {
+        public abstract UnityEngine.Vector2 GetDirectionOnInputDetection();
+    }
+}
