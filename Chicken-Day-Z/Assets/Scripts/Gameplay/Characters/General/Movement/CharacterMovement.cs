d05cd91a614b03ae55684578a2bb6ae487a878fc
facundo <facuwilli@gmail.com>
2022-09-18 15:53:35 -0300

    Added abstract class with basic informacion all characters need to move.

---
+++ Chicken-Day-Z/Assets/Scripts/Gameplay/Characters/General/Movement/CharacterMovement.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+using ChickenDayZ.Gameplay.Interfaces;
+
+namespace ChickenDayZ.Gameplay.Characters.Movement 
+{
+    public abstract class CharacterMovement : IMoves
+    {
+        private Rigidbody2D _objectRigidbody2D;        
+
+        private Vector2 _moveDirection;
+
+        private float _moveSpeed;
+
+        public Rigidbody2D ObjectRigidbody2D 
+        {
+            get 
+            {
+                return _objectRigidbody2D;
+            }
+        }
+
+        public Vector2 MoveDirection 
+        {
+            set 
+            {
+                _moveDirection = value;
+            }
+            get 
+            {
+                return _moveDirection;
+            }
+        }
+
+        public float MoveSpeed 
+        {
+            set 
+            {
+                _moveSpeed = value;
+            }
+            get 
+            {
+                return _moveSpeed;
+            }
+        }        
+
+        public CharacterMovement(Rigidbody2D objectRigidbody2D, float moveSpeed) 
+        {
+            _objectRigidbody2D = objectRigidbody2D;
+
+            _moveDirection = Vector2.zero;
+
+            _moveSpeed = moveSpeed;
+        }
+
+        public abstract void CalculateMoveDirection();
+
+        public abstract void Move();
+    }
+}
