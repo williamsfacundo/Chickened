65e21b1d929a28d9a6f9b02fca22acdc4bda9462
facundo <facuwilli@gmail.com>
2022-09-20 12:10:28 -0300

    Added timer class that will be usegfull in many part of the code.

---
+++ Chicken-Day-Z/Assets/Scripts/General/Timer.cs
@@ -0,0 +1,66 @@
+namespace ChickenDayZ.General
+{
+    public class Timer
+    {
+        private float _time;
+
+        private float _countDown;
+
+        public float Time
+        {
+            set
+            {
+                _time = value;
+            }
+            get
+            {
+                return _time;
+            }
+        }
+
+        public float CountDown
+        {
+            set
+            {
+                _countDown = value;
+            }
+            get
+            {
+                return _countDown;
+            }
+        }
+
+        public bool TimerFinished
+        {
+            get
+            {
+                return _countDown <= 0f;
+            }
+        }
+
+        public Timer(float time) 
+        {
+            _time = time;
+
+            ResetTimer();
+        }
+
+        public void ResetTimer()
+        {
+            _countDown = _time;
+        }
+
+        public void DecreasTimer()
+        {
+            if (_countDown > 0f)
+            {
+                _countDown -= UnityEngine.Time.deltaTime;
+
+                if (_countDown <= 0f)
+                {
+                    _countDown = 0f;
+                }
+            }
+        }
+    }
+}
