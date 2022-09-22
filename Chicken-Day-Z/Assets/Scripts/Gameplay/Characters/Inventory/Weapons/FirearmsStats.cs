64d27dc9f0effb3f3cdeca0aeb2737dbdd7e77d8
facundo <facuwilli@gmail.com>
2022-09-22 17:38:43 -0300

    Added a firearm stats container namespace.

---
+++ Chicken-Day-Z/Assets/Scripts/Gameplay/Characters/Inventory/Weapons/FirearmsStats.cs
@@ -0,0 +1,20 @@
+namespace ChickenDayZ.Gameplay.Characters.Inventory.Weapons
+{
+    public struct FirearmsStats
+    {
+        public struct Rifle 
+        {
+            public const short MaxAmmo = 120;
+
+            public const short FireCapacity = 1;
+
+            public const short MagazineSize = 25;
+
+            public const float FireRate = 0.6f;
+
+            public const float Damage = 10f;
+
+            public const float ReloadTime = 2.5f;
+        }        
+    }
+}
