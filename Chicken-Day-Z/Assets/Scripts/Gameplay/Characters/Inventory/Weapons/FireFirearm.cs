d6e5b0d8eb684d4e2ceb3dbf58b945dd551a6c6d
facundo <facuwilli@gmail.com>
2022-09-27 09:35:32 -0300

    Changed the logic when storing the projectiles data.

--- Chicken-Day-Z/Assets/Scripts/Gameplay/Characters/Inventory/Weapons/FireFirearm.cs
+++ Chicken-Day-Z/Assets/Scripts/Gameplay/Characters/Inventory/Weapons/FireFirearm.cs
@@ -7,7 +7,9 @@ namespace ChickenDayZ.Gameplay.Characters.Inventory.Weapons
 {
     public class FireFirearm
     {
-        private GameObject[] _projectiles;
+        private ProjectileMovement[] _projectiles;
+
+        private GameObject _character;
 
         private Timer _timer;
 
@@ -15,21 +17,27 @@ namespace ChickenDayZ.Gameplay.Characters.Inventory.Weapons
 
         private Canyon _canyon;
 
-        public FireFirearm(GameObject projectilePrefab, Charger charger, Canyon canyon)
+        private Camera _camera;
+
+        public FireFirearm(GameObject projectilePrefab, Charger charger, Canyon canyon, GameObject character)
         {
+            _character = character;
+
             _charger = charger;
 
             _canyon = canyon;
 
             _timer = new Timer(_canyon.FireRate);
 
-            _projectiles = new GameObject[_charger.ChargerMaxAmmo];
+            _projectiles = new ProjectileMovement[_charger.ChargerMaxAmmo / 2];            
 
             for (short i = 0; i < _projectiles.Length; i++) 
             {
-                _projectiles[i] = GameObject.Instantiate<GameObject>(projectilePrefab);
+                _projectiles[i] = GameObject.Instantiate<GameObject>(projectilePrefab).GetComponent<ProjectileMovement>();
 
-                _projectiles[i].SetActive(false);
+                _projectiles[i].SetProjectile(canyon.BulletMoveSpeed, canyon.Range);
+
+                _projectiles[i].gameObject.SetActive(false);
             }
         }
 
@@ -39,23 +47,43 @@ namespace ChickenDayZ.Gameplay.Characters.Inventory.Weapons
             {
                 for (short i = 0; i < _projectiles.Length; i++)
                 {
-                    if (!_projectiles[i].activeSelf) 
+                    if (!_projectiles[i].gameObject.activeSelf) 
                     {
-                        _projectiles[i].SetActive(true);
+                        _projectiles[i].gameObject.SetActive(true);
+
+                        _projectiles[i].gameObject.transform.position = _character.transform.position;
+
+                        _projectiles[i].Direction = CalculateProjectileDirection(_character.transform.position);
 
                         _timer.ResetTimer();
 
-                        _charger.DecreaseCharger(_canyon.FireCapacity);
+                        _charger.DecreaseCharger(_canyon.FireCapacity);                        
 
                         break;
                     }                                        
                 }
-            }           
+            }            
         }
 
         public void FireFirearmCoolDown() 
         {
             _timer.DecreaseTimer();
+        }
+
+        private Vector3 CalculateProjectileDirection(Vector3 playerPosition) 
+        {
+            if (_camera == null) 
+            {
+                _camera = Camera.main;
+            }
+
+            Vector3 direction = _camera.ScreenToWorldPoint(Input.mousePosition) - playerPosition;
+
+            direction.z = 0f;
+
+            Vector3 normalizedDirection = Vector3.Normalize(direction);
+
+            return normalizedDirection;
         }
     }
 }
