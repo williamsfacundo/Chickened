20ec718d6a561de05c809145427b63d297c25e5e
facundo <facuwilli@gmail.com>
2022-09-27 15:06:05 -0300

    Overrided the chicken health method to get chicken type.

--- Chicken-Day-Z/Assets/Scripts/Gameplay/Characters/Health/ChickenHealth.cs
+++ Chicken-Day-Z/Assets/Scripts/Gameplay/Characters/Health/ChickenHealth.cs
@@ -1,12 +1,20 @@
 using ChickenDayZ.Gameplay.Controllers;
+using ChickenDayZ.Gameplay.Enumerators;
 
 namespace ChickenDayZ.Gameplay.Characters.Health 
 {
     public class ChickenHealth : ObjectHealth
     {
+        public override CharacterTypeEnum GetCharacterType()
+        {
+            return CharacterTypeEnum.CHICKEN;
+        }
+
         public override void HealthReachedZero() 
         {
             GameplayResetter.ResetGameplay();            
         }        
+
+
     }
 }
