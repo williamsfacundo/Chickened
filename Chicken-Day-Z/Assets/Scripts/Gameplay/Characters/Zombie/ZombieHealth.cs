a2799e3fae29312af23098b574a4289b439df766
facundo <facuwilli@gmail.com>
2022-09-28 20:51:14 -0300

    Added zombie health.

--- Chicken-Day-Z/Assets/Scripts/Gameplay/Characters/Zombie/ZombieHealth.cs
+++ Chicken-Day-Z/Assets/Scripts/Gameplay/Characters/Zombie/ZombieHealth.cs
@@ -2,28 +2,45 @@ using UnityEngine;
 
 using ChickenDayZ.Gameplay.Characters.Chicken.Score;
 using ChickenDayZ.Gameplay.Enumerators;
+using ChickenDayZ.Gameplay.Characters.Health;
 
-namespace ChickenDayZ.Gameplay.Characters.Health 
+namespace ChickenDayZ.Gameplay.Characters.Zombie 
 {
     public class ZombieHealth : ObjectHealth
     {
         [SerializeField] private float _pointsGivenWhenDie;
         
-        private ChickenScore _chickenScore;        
+        private ChickenScore _chickenScore;             
+        
+        public static short ZombiesInstances = 0; 
 
         void Awake()
         {
-            _chickenScore = FindObjectOfType<ChickenScore>();
+            _chickenScore = FindObjectOfType<ChickenScore>();           
+        }
+
+        private void Start()
+        {
+            ZombiesInstances++;
         }
 
         public override void HealthReachedZero()
         {
             _chickenScore?.AddScore(_pointsGivenWhenDie);
+
+            DeactivateZombie();
         }
 
         public override CharacterTypeEnum GetCharacterType()
         {
             return CharacterTypeEnum.ZOMBIE;
+        }
+
+        public void DeactivateZombie() 
+        {
+            Destroy(gameObject);
+
+            ZombiesInstances--;
         }
     }
 }
