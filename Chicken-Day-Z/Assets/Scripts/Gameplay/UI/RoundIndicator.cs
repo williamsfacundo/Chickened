79ad97d3ad1e72d1e570df4cf11083143f4aec6e
facundo <facuwilli@gmail.com>
2022-10-04 18:22:17 -0300

    Replaced end game timer indicator with round indicator.

---
+++ Chicken-Day-Z/Assets/Scripts/Gameplay/UI/RoundIndicator.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using TMPro;
+
+using ChickenDayZ.Gameplay.Characters.Zombie;
+
+namespace ChickenDayZ.UI 
+{
+    public class RoundIndicator : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _showRoundText;
+
+        private ZombiesSpawner _zombiesSpawner;
+
+        private void Awake()
+        {
+            _zombiesSpawner = FindObjectOfType<ZombiesSpawner>();
+
+            if (_zombiesSpawner == null) 
+            {
+                Debug.LogError("Zombie spawner couldnt be found!");
+            }
+        }
+
+        void Start()
+        {
+            UpdateRoundText();
+        }
+
+        void OnEnable()
+        {
+            _zombiesSpawner.OnRoundChanged += UpdateRoundText;
+        }
+
+        void OnDisable()
+        {
+            _zombiesSpawner.OnRoundChanged -= UpdateRoundText;
+        }
+
+        private void UpdateRoundText() 
+        {
+            _showRoundText.text = "Round " + _zombiesSpawner.Round;
+        }
+    }
+}
