005b823e9f64b70745fe69db4d8c3e3089af3358
facundo <facuwilli@gmail.com>
2022-10-17 10:16:40 -0300

    Added tree object.

---
+++ Chicken-Day-Z/Assets/Scripts/Gameplay/Main Objects/Buildings/TreeObject.cs
@@ -0,0 +1,22 @@
+using ChickenDayZ.Gameplay.MainObjects.Enumerators;
+
+namespace ChickenDayZ.Gameplay.MainObjects 
+{
+    public class TreeObject : BuildingObject
+    {
+        private TreeObject() : base(BuildingObjectTypeEnum.TREE)
+        {
+
+        }
+
+        public override void GameplayResset()
+        {
+
+        }
+
+        public override void RoundResset()
+        {
+
+        }
+    }
+}
