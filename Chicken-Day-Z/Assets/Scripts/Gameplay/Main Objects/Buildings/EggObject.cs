cc5e293dddaf1d9b3d25351f930156b79ce653ff
facundo <facuwilli@gmail.com>
2022-10-17 10:47:07 -0300

    Added ObjectHealth to the EggObject.

--- Chicken-Day-Z/Assets/Scripts/Gameplay/Main Objects/Buildings/EggObject.cs
+++ Chicken-Day-Z/Assets/Scripts/Gameplay/Main Objects/Buildings/EggObject.cs
@@ -1,22 +1,37 @@
 using ChickenDayZ.Gameplay.MainObjects.Enumerators;
+using ChickenDayZ.Gameplay.StatsScripts;
 
 namespace ChickenDayZ.Gameplay.MainObjects 
 {
     public class EggObject : BuildingObject
     {
+        private ObjectHealth _health;
+
+        public ObjectHealth Health 
+        {
+            set 
+            {
+                _health = value;
+            }
+            get 
+            {
+                return _health;
+            }
+        }
+
         private EggObject() : base(BuildingObjectTypeEnum.EGG)
         {
-
+                        
         }
 
         public override void GameplayResset()
         {
-
+            _health.GameplayResset();
         }
 
         public override void RoundResset()
         {
-
+            _health.RoundResset();
         }
     }
 }
