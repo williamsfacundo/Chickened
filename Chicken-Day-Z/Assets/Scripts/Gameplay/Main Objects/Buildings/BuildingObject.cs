5206441c8fa2e722e2312081e2c313a1912cc395
facundo <facuwilli@gmail.com>
2022-10-17 12:00:51 -0300

    Added BuildingObjects namespace.

--- Chicken-Day-Z/Assets/Scripts/Gameplay/Main Objects/Buildings/BuildingObject.cs
+++ Chicken-Day-Z/Assets/Scripts/Gameplay/Main Objects/Buildings/BuildingObject.cs
@@ -1,6 +1,6 @@
 using ChickenDayZ.Gameplay.MainObjects.Enumerators;
 
-namespace ChickenDayZ.Gameplay.MainObjects 
+namespace ChickenDayZ.Gameplay.MainObjects.BuildingObjects 
 {
     public abstract class BuildingObject : MainObject
     {
