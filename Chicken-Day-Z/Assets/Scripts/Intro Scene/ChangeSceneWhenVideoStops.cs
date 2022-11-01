f4234ab95b87b82c15652d155f97915984557d52
facundo <facuwilli@gmail.com>
2022-11-01 11:26:17 -0300

    Added intro scene.

---
+++ Chicken-Day-Z/Assets/Scripts/Intro Scene/ChangeSceneWhenVideoStops.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+using ChickenDayZ.General;
+
+public class ChangeSceneWhenVideoStops : MonoBehaviour
+{
+    private Timer _timer;
+
+    void Start()
+    {
+        _timer = new Timer(58f);        
+    }
+
+    void Update()
+    {
+        _timer.DecreaseTimer();
+
+        if (_timer.TimerFinished) 
+        {
+            ChangeSceneToGameplay();
+        }
+    }
+
+    public void ChangeSceneToGameplay() 
+    {
+        SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
+    }
+}
