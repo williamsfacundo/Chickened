bcc7dacce4203bbbbd43e8e8c5d88d98519006e0
facundo <facuwilli@gmail.com>
2022-11-03 23:40:37 -0300

    There is a gameplay working.

--- Chicken-Day-Z/Assets/Scripts/Gameplay/Main Objects/Clases Scripts/Buildings/EggObject.cs
+++ Chicken-Day-Z/Assets/Scripts/Gameplay/Main Objects/Clases Scripts/Buildings/EggObject.cs
@@ -1,9 +1,31 @@
+using UnityEngine;
+
+using ChickenDayZ.Gameplay.Health;
+using ChickenDayZ.Gameplay.Controllers;
 using ChickenDayZ.Gameplay.MainObjects.Enumerators;
 
 namespace ChickenDayZ.Gameplay.MainObjects.Buildings
 {
+    [RequireComponent(typeof(ObjectHealth))]
     public class EggObject : BuildingObject 
     {
+        private ObjectHealth _objectHealth;
+
+        void Awake()
+        {
+            _objectHealth = GetComponent<ObjectHealth>();
+        }
+
+        void OnEnable()
+        {
+            _objectHealth.OnHealthReachedZero += GameplayResetter.ResetGameplay;            
+        }
+
+        void OnDisable()
+        {
+            _objectHealth.OnHealthReachedZero -= GameplayResetter.ResetGameplay;
+        }
+
         private EggObject() : base(BuildingObjectTypeEnum.EGG)
         {
 
