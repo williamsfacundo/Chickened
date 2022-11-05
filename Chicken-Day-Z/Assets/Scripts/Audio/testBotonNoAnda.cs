4f85bb35a285c996a01f1503afb8b3938975d8fb
Leonardo Viegas <leojviegas@hotmail.com>
2022-11-05 02:50:29 -0300

    Arreglé el error de pullear main, resetando audio a un commit ant

---
+++ Chicken-Day-Z/Assets/Scripts/Audio/testBotonNoAnda.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testBotonNoAnda : MonoBehaviour
+{
+    public void ClickEnBoton() {
+        Debug.Log("se clickeo el mouse");
+    }
+}
