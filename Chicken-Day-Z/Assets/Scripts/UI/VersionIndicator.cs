91534426cd4b0d9d4e001e63aefbc5c0f5684f27
facundo <facuwilli@gmail.com>
2022-11-24 07:48:02 -0300

    Show game version in main menu.

--- Chicken-Day-Z/Assets/Scripts/UI/VersionIndicator.cs
+++ Chicken-Day-Z/Assets/Scripts/UI/VersionIndicator.cs
@@ -5,11 +5,11 @@ namespace ChickenDayZ.UI
 {
     public class VersionIndicator : MonoBehaviour
     {
-        /*[SerializeField] private TMP_Text _showVersionText;
+        [SerializeField] private TMP_Text _showVersionText;
 
         void Awake()
         {
             _showVersionText.text = "Version " + Application.version;
-        }*/
+        }
     }
 }
